412e0a78cf3c8d70b66f83cb53367328de5a4e19
rosdebiasi <rosanadebiasi@gmail.com>
2022-07-04 18:07:38 -0300

    login via google

---
+++ src/main/java/com/checkapp/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.checkapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http
+                .authorizeRequests().antMatchers("/", "/inspecao**", "/error**").permitAll()
+                .anyRequest().authenticated()
+                .and().logout().invalidateHttpSession(true).clearAuthentication(true).deleteCookies("JSESSIONID").logoutSuccessUrl("/")
+                .and().oauth2Login().defaultSuccessUrl("/home.jr");
+
+    }
+
+}
